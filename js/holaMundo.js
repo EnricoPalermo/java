a3929420097a02a37f088ea0df7cc9ee85af35a0
¨EnricoPalermo¨ <epepalermo78@gmail.com>
2021-08-17 16:35:18 -0300

    practiva realizada

---
+++ js/holaMundo.js
@@ -0,0 +1,24 @@
+// esto es un comentario simple
+/* esto es 
+un comentario de
+multiples lineas*/
+console.log("Hola mundo")
+
+//mostrar un mensaje en la pagina HTML
+document.write("Este es mi primer mensaje")
+
+//crear variables var - let - const
+
+let palabraBuscada = "Hola";
+
+document.write("<br>Variable palabra buscada: "+ palabraBuscada)
+
+// modificar el valor de una variable
+
+palabraBuscada = "pagina de blog";
+
+document.write("<br>Variable palabra buscada: "+ palabraBuscada)
+
+const comision = 17
+
+document.write("<br>Constante comision: "+ comision);
