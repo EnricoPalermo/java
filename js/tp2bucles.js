2f4c83c27431d3db0a7df27a290902df1539f797
¨EnricoPalermo¨ <epepalermo78@gmail.com>
2021-09-09 16:43:09 -0300

    ejercicio 16 tp2 agregado en dev

--- js/tp2bucles.js
+++ js/tp2bucles.js
@@ -150,17 +150,12 @@ for(let x=0;x<texto.length;x++) {
    document.write("La palabra " + texto + " contiene " + contador + " vocales");*/
 
         //ejercicio 16
-let palabra = "Perro";
-let invertirPalabra = palabra => {
-let nuevaCadena = ""
-for (let x = palabra.length-1; x>= 0; x--) {
-    nuevaCadena = palabra[x];
-            }
-    return nuevaCadena;
-        }
-        
 
-document.write(`Original ${palabra}, invertida ${invertirPalabra(palabra)}`);
+/*let palabra = prompt('ingrese una palabra');
+for(let x = palabra.length-1; x >= 0; x--){
+    console.log(palabra[x])
+    document.write(palabra[x]+"-")
+}*/
 
 
 
